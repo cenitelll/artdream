9a1ee22147579a04ec84c94f79154d51fd6724f2
Theconnoiser <cenitelll@gmail.com>
2023-04-19 18:49:58 +0300

    to the infinite scroll

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,4 +3,15 @@ class Pin < ApplicationRecord
   belongs_to :user
 
   has_one_attached :pin_image
+  validate :correct_pin_image
+
+  private
+
+  def correct_pin_image
+    if pin_image.attached? && !pin_image.content_type.in?(%w(image/jpeg image/png image/gif image/webp))
+        errors.add(:pin_image, 'needs to be a JPEG, PNG, GIF or WEBP')
+    elsif pin_image.attached? == false
+        errors.add(:pin_image, 'must have an image attached.')
+    end
+  end
 end
